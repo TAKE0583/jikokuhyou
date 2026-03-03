09f1e051c0e74c21b0d0697f04887427cf2c3952
TAKE0583 <tengzewuhu351@gmail.com>
2026-03-03 10:54:22 +0900

    main.htmlの名称をindex.htmlに変更。それに伴い参照されていた部分の名称も変更

--- serve.bat
+++ serve.bat
@@ -4,5 +4,5 @@ echo   時刻表サーバーを起動中...
 echo   http://localhost:8080 でアクセスできます
 echo   終了するには Ctrl+C を押してください
 echo ========================================
-start http://localhost:8080/main.html
+start http://localhost:8080/index.html
 python -m http.server 8080
